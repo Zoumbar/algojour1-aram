1ddc1662b5f66a23f20400b5b5affc31794b84c3
Ar4m <a-j.tosbath@orange.fr>
2021-01-18 14:28:07 +0100

    Bubble_sort + readme

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,24 @@
+import sys
+arr = sys.argv[1].split(';')
+liste = []
+
+x = 0
+while x < len(arr):
+    liste.append(float(arr[x]))
+    x = x + 1
+
 def bubble_sort(liste):
+    n = 0
     for i in range(len(liste)):
         for j in range(len(liste) - 1):
             if liste[j] > liste[j+1]:
                 liste[j], liste[j+1] = liste[j+1], liste[j]
+        n += 1
+    print("Nb d'itération :", n)
 
-liste = [1, -2, 3.5, 4]
+print("Série : ", liste)
+
 bubble_sort(liste)
-print(liste)
+
+print("Résultat : ", liste)
+
