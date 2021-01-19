274c58930cc1bbf600878ee0be1d32c35cee820a
Esteban <Esteban@isee-u.fr>
2021-01-19 10:09:08 +0100

    Classe blockchain

---
+++ jour2/blockOfBlockchain.py
@@ -0,0 +1,9 @@
+class Block: 
+    def __init__(self, identifier, previousHash, hashOfBody, bockHash, timestamp, nonce):
+        self.identifier = identifier
+        self.previousHash = previousHash
+        self.hashOfBody = hashOfBody
+        self bockHash = bockHash
+        self.timestamp = timestamp
+        self.nonce = nonce
+
