ffa34aa4848af936ecd34a22566a4dec63cb0ca3
Ar4m <a-j.tosbath@orange.fr>
2021-01-19 16:33:42 +0100

    block

---
+++ jour2/block.py
@@ -0,0 +1,42 @@
+from hashlib import sha256
+from datetime import datetime
+
+def calculateHash(block):
+        bloc = str(block.index) + str(block.previousHash) + str(block.timestamp) + str(block.data) + str(block.nonce)
+        return(sha256(bloc.encode('utf-8')).hexdigest())
+
+class Block:
+
+    def __init__(self, index, previousHash, data, timestamp, hash, nonce):
+        self.index = index
+        self.previousHash = previousHash
+        self.data = data
+        self.timestamp = timestamp
+        self.nonce = nonce
+        self.hash = calculateHash(self)
+
+    def check(self):
+         while self.hash[0:2] != "00":
+            self.nonce += 1
+            newHash = calculateHash(self)
+            self.hash = newHash
+
+
+first = Block(0, "none", "First Block", datetime.now(), "", 0)
+first.check()
+print(first.hash)
+print("nonce: ", first.nonce)
+
+
+#if str[0,3] == "000"
+
+#bchain = Blockchain(3)
+#
+#    blockn1 = bchain.newBlock("Second Block")
+#    bchain.addBlock(blockn1)
+#
+#    blockn2 = bchain.newBlock("Third Block")
+#    bchain.addBlock(blockn2)
+#
+#    blockn3 = bchain.newBlock("Fourth Block")
+#    bchain.addBlock(blockn3)
--- jour2/blockOfBlockchain.py
+++
@@ -1,8 +0,0 @@
-class Block: 
-    def __init__(self, index, previousHash, data, timestamp, blockHash, nonce):
-        self.index = index
-        self.previousHash = previousHash
-        self.data = data
-        self.timestamp = timestamp
-        self.blockHash = blockHash
-        self.nonce = nonce
