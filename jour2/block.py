8809343308b72a22151d1d7c6f7c22e858f47576
Ar4m <a-j.tosbath@orange.fr>
2021-01-20 11:53:43 +0100

    Update block.py

--- jour2/block.py
+++ jour2/block.py
@@ -1,52 +1,64 @@
 from hashlib import sha256
 from datetime import datetime
 
-def calculateHash(block):
-        bloc = str(block.index) + str(block.previousHash) + str(block.timestamp) + str(block.data) + str(block.nonce)
-        return(sha256(bloc.encode('utf-8')).hexdigest())
+
+def calculateHash(self):
+    bloc = (
+        str(self.index)
+        + str(self.previousHash)
+        + str(self.timestamp)
+        + str(self.data)
+        + str(self.nonce)
+    )
+    return sha256(bloc.encode("utf-8")).hexdigest()
+
 
 class Block:
-
     def __init__(self, index, previousHash, data, timestamp, hash, nonce):
         self.index = index
         self.previousHash = previousHash
         self.data = data
         self.timestamp = timestamp
-        self.nonce = 0
+        self.nonce = nonce
         self.hash = calculateHash(self)
 
     def check(self):
-         while self.hash[0:2] != "00":
-            self.nonce += 1
-            self.hash = calculateHash(self)
-            
+        while self.hash[0:2] != "00":
+            self.nonce = self.nonce + 1
+            newHash = calculateHash(self)
+            self.hash = newHash
+
 
 class Blockchain:
-
     def __init__(self):
-        self.blockchain = []
-        first = Block(0, "none", "First Block", datetime.now(), "", 0)
-        first.check()
-        self.blockchain.append(first)
+        self.blocks = []
 
-    def newBlock(self, data):
-        previous = self.blockchain[-1]
-        return(Block(previous.index + 1, previous.hash, data, datetime.now(), hash, nonce))
+        firstBlock = Block(0, "none", "First Block", datetime.now(), "", 0)
+        firstBlock.check()
+        self.blocks.append(firstBlock)
 
 
+    def AddBlock(self, blockToAdd):
+        x = 0
+        while x < blockToAdd:
+            Index = self.blocks[x].index + 1
+            PreviousHash = self.blocks[x].hash
+            NewBlock = Block(Index, PreviousHash, "Block", datetime.now(), "", 0  )
+            NewBlock.check()
+            self.blocks.append(NewBlock)
+            x = x + 1
 
 
-#print(first.hash)
-#print("nonce: ", first.nonce)
+blockchain = Blockchain()
+blockchain.AddBlock(2)
 
-
-#bchain = Blockchain(3)
-#
-#    blockn1 = bchain.newBlock("Second Block")
-#    bchain.addBlock(blockn1)
-#
-#    blockn2 = bchain.newBlock("Third Block")
-#    bchain.addBlock(blockn2)
-#
-#    blockn3 = bchain.newBlock("Fourth Block")
-#    bchain.addBlock(blockn3)
+for block in blockchain.blocks:
+    print (
+    str("Block index : " + str(block.index) + 
+    "\nPrevious hash : " + str(block.previousHash) +
+    "\nTimestamp : " + str(block.timestamp) +
+    "\nData : " + str(block.data) +
+    "\nHash : " + str(block.hash) +
+    "\nNonce : " + str(block.nonce) +
+    "\n"
+    ))
